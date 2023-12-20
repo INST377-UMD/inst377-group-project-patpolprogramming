533ff35c9b56007afa294eaa91f0906affb6b187
Patrick Polglase <ppolglas@umd.edu>
2023-12-19 21:21:51 -0500

    updated files to include the proper js pathway in each html page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser')
 const express = require("express")
+const supabaseClient = require('@supabase/supabase-js')
 const app = express()
 const port = 4000
 
@@ -7,6 +8,40 @@ app.use(bodyParser.json())
 
 // this allows us to use all files in the public folder
 app.use(express.static(__dirname + '/public'))
+
+// supabase DB for user information
+const supabaseUrl = 'https://lmomegzjxijkiqxhjjfz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxtb21lZ3pqeGlqa2lxeGhqamZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1NTUzNTAsImV4cCI6MjAxNzEzMTM1MH0.5TKzmamlai1ZLgVJGcpSAMFVty7rJA7mdxeAf1K3ODM'
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+
+// // GET, which serves to retrieve the user database 
+// app.get('/users', async (req, res) => {
+//     console.log(`Getting User`)
+
+//     const {data, error} = await supabase
+//         .from('User')
+//         .select();
+
+//     if(error) {
+//         console.log(error)
+//     } else if(data) {
+//         res.send(data)
+//     }
+// })
+// POST, updates the supabase database with user data
+app.get('/users', async (red, res) => {
+    console.log('getting User')
+
+    const {data, error} = await supabase
+        .from('User')
+        .select();
+
+    if(error) {
+        console.log(error)
+    } else if(data) {
+            res.send(data)
+        }
+    })
 
 // GET, which serves the index.html page
 app.get("/home.html", (req, res) => {
@@ -26,11 +61,6 @@ app.get("/data.html", (req, res) => {
 // GET, which serves the help.html page
 app.get("/help.html", (req, res) => {
     res.sendFile("public/html/help.html", { root: __dirname })
-})
-
-// example GET endpoint, which returns the current date
-app.get("/current_date", (req, res) => {
-    res.send(new Date())
 })
 
 app.listen(port)
